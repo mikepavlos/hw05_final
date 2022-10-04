334d82bea30f02bfdcd4b399d428d35de6267751
Михаил Павлов <pavlovichmihaylovich@yandex.ru>
2022-10-04 16:45:11 +0300

    Ver 5.2.2 pythonanywhere

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '#!6qv^ao7w6jtn(d2%g=l^0@70f)&obimpcz4_whjem_#p)=%$'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
